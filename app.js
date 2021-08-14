62ffe4537e5f0895799c9311ad1c99a586656aaa
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-14 18:28:02 +0100

    Initializing the express server, connected to DB; installed main dependencies

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+require("dotenv/config");
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors({ origin: ["http://localhost:3000"], httpOnly: true }));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongodb Atlas.");
+    app.listen(PORT, () => console.log(`Server listeening on port ${PORT}`));
+  })
+  .catch((error) => console.log(error));
