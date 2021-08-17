606734d1415adec2535b6ec046e6dd7298148f96
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-17 22:34:44 +0100

    Created Post model and posts endpoints in the controller

---
+++ src/controllers/posts.js
@@ -0,0 +1,67 @@
+const Post = require("../models/Post");
+
+const getAllPosts = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getMyPosts = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getPostsByAuthor = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const createPost = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getPost = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAllPosts,
+  getMyPosts,
+  getPostsByAuthor,
+  createPost,
+  getPost,
+  updatePost,
+  deletePost,
+};
---
+++ src/models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "users",
+    },
+    title: { type: String, required: true, trim: true },
+    content: { type: String, required: true, default: "" },
+    published: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("posts", postSchema);
