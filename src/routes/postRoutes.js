f978cf1644c5559f4685472c83c37e6985053512
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-21 22:16:44 +0100

    Changed the post routes order

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -9,10 +9,10 @@ const {
   deletePost,
 } = require("../controllers/posts");
 
+router.get("/posts/:slug", getPost);
 router.post("/posts", auth, createPost);
 router.get("/posts/all", getAllPosts);
 router.get("/posts/my-posts", auth, getMyPosts);
-router.get("/posts/:slug", getPost);
 router.put("/posts/:slug", auth, updatePost);
 router.delete("/posts/:slug", auth, deletePost);
 
