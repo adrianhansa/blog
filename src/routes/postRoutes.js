d7bb6447e6bfa76146504ff668c0e9b39faaf765
Adrian Hansa <adrian.hansa@yahoo.com>
2021-08-23 17:22:16 +0100

    Fixed route for togglePublishPost

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -16,7 +16,7 @@ router.get("/posts", getAllPosts);
 router.get("/posts/:slug", getPost);
 router.get("/admin/posts", auth, getMyPosts);
 router.get("/admin/posts/:slug", auth, getMyPost);
-router.put("/admin/posts/:slug",togglePublishPost)
+router.put("/admin/posts/:slug",auth,togglePublishPost)
 router.put("/posts/:slug", auth, updatePost);
 router.delete("/posts/:slug", auth, deletePost);
 
