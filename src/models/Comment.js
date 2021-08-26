e589897c02dc8888a61fc5c6669f363f6ebcb4e0
Adrian Hansa <adrian.hansa@yahoo.com>
2021-08-26 18:12:54 +0100

    Created Comment model

---
+++ src/models/Comment.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "posts",
+    },
+    name: { type: String, required: true, trim: true, maxLength: 50 },
+    email: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("comments", commentSchema);
